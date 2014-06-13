d4acb8fb9ff80fb41ce07c6acc528af530a8325a
RoronoaMao <amriverosd@gmail.com>
2014-06-13 17:09:09 -0500

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -1,23 +1,28 @@
+#
 print "I will now count my chickens:"
 
+#Uso de operacionde matemáticas durante la impresión 
 print "Hens", 25 + 30 / 6
+
+#Modulo de un número.
 print "Roosters", 7 % 9
 
 print "Now I will count the eggs:"
-
+#Jerarquia de las operaciones.
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
 print "Is it true that 3 + 2 < 5 - 7?"
 
 print 3 + 2 < 5 - 7
 
+#Más operaciones matemáticas. 
 print "What is 3 + 2?", 3 + 2
 print "What is 5 - 7?", 5 - 7
 
 print "Oh, that's why it's False."
-
 print "How about some more."
 
+#Operaciondes de Logica, se devuelve un valor booleano. 
 print "Is it greater?", 5 > -2
 print "Is it greater or equal?", 5 >= -2
 print "Is it less or equal?", 5 <= -2
