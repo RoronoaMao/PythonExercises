b9d4a5bbafafec392a501e9824d9913acd52eb0e
RoronoaMao <amriverosd@gmail.com>
2014-06-13 16:59:03 -0500

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,8 +1,5 @@
-print "Hello World!"
-print "Hello Again"
-print "I like typing this."
-print "This is fun."
-#print 'Yay! Printing.'
 print "I'd much rather you 'not'."
-#print 'I "said" do not touch this.' 
+#comando para escribir
 print 'Hola mundo en python :D' 
+print 'Hola Num2' 
+print 'Fin' 
