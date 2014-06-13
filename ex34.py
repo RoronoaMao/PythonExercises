629a98e9a82871203008d7df80709ea7e33c6d50
Mauricio <mauricio@server.ud>
2014-06-13 16:11:58 -0500

    first tuto

---
+++ ex34.py
@@ -0,0 +1,5 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+i = 1;
+for animal in animals:
+    print "The animal number %d is %s" % (i, animal)
+    i += 1
---
+++ ex4.py
@@ -0,0 +1,20 @@
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+#the number of car.
+print "There are", cars, "cars available."
+#The number of drivers
+print "There are only", drivers, "drivers available."
+#print the cars_not_drivven value, it's the diffrence 
+#between cars and drivers. 
+print "There will be", cars_not_driven, "empty cars today."
+#The factor between cars_drive and space_in_cars. 
+print "We can transport", carpool_capacity, "people today."
+print "We have", passengers, "to carpool today."
+print "We need to put about", average_passengers_per_car, "in each car."
